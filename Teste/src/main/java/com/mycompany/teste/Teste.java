4b4a16ec06faf485bee100862fa836d75b808e3d
MATHEUS MIOSSI SCHMIDT <m.schmidt@labcetec.net>
2024-08-09 21:06:52 -0300

    Exercicios Dia 09

--- Teste/src/main/java/com/mycompany/teste/Teste.java
+++ Teste/src/main/java/com/mycompany/teste/Teste.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.mavenproject1;
+package com.mycompany.teste;
 
 /**
  *
  * @author m.schmidt
  */
-public class Mavenproject1 {
+public class Teste {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
