82e3854ba16c14cc7c132874b1d3b52da09626bb
Matheus Miossi Schmidt <matheus.schmidt@aluno.senai.br>
2024-08-11 09:39:07 -0300

    Testando Repo

---
+++ PROJETO_9_Matheus/src/main/java/com/mycompany/projeto_9_matheus/PROJETO_9_Matheus.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto_9_matheus;
+
+/**
+ *
+ * @author mathe
+ */
+public class PROJETO_9_Matheus {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
